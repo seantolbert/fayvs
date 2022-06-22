11353744e9ce4a537a7de3462ba86d744986431c
Sean Tolbert <stolbert4150@gmail.com>
2022-06-22 16:39:33 -0400

    functional filter

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -15,14 +15,31 @@ export default function Dashboard() {
     setCurrentFilter(newFilter);
   };
 
+  const actz = acts
+    ? acts.filter((act) => {
+        switch (currentFilter) {
+          case "all":
+            return true;
+          case "Course":
+          case "App":
+          case "Repo":
+          case "Post":
+          case "Resource":
+            return act.category === currentFilter;
+          default:
+            return true;
+        }
+      })
+    : null;
+
   return (
     <div>
       <Typography variant="h2" component="h1">
-        FAYVS
+        FAYVS of {user.email}
       </Typography>
 
       <ActFilter currentFilter={currentFilter} changeFilter={changeFilter} />
-      {acts && <ActList acts={acts}/>}
+      {acts && <ActList acts={actz} />}
       <MobileNav />
     </div>
   );
